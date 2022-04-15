22a321dc8a529b8de873b38539a75e39a6b0dc0a
AlexRozaLopes <alexrozaestudos@gmail.com>
2022-04-15 12:07:30 -0300

    add seguranca com jwt usando keycloak

--- src/main/java/com/sisconsultoria/challenge/controller/StudentsController.java
+++ src/main/java/com/sisconsultoria/challenge/controller/StudentsController.java
@@ -5,6 +5,7 @@ import com.sisconsultoria.challenge.service.StudentsService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,16 +17,19 @@ public class StudentsController {
     private final StudentsService studentsService;
 
     @GetMapping
+    @RolesAllowed("admin")
     public List<StudentsModel> getAllStudents(){
         return studentsService.findAllStudents();
     }
 
     @GetMapping("/{id}")
+    @RolesAllowed({"admin","user"})
     public StudentsModel getStudent(@PathVariable UUID id){
         return studentsService.findStudentById(id);
     }
 
     @PostMapping
+    @RolesAllowed("admin")
     public void postStudent(@RequestBody StudentsModel studentsModel){
         studentsService.postStudent(studentsModel);
     }
